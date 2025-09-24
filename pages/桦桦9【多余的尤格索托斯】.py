6460b8cd58cfbd82f3aa1243309867a886b1f7b3
kingspround <100604738+kingspround@users.noreply.github.com>
2025-09-24 23:18:49 +0800

    Update 桦桦9【多余的尤格索托斯】.py

--- pages/桦桦9【多余的尤格索托斯】.py
+++ pages/桦桦9【多余的尤格索托斯】.py
@@ -60,7 +60,7 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-  model_name="gemini-2.0-flash-exp",
+  model_name="gemini-2.5-flash-preview-05-20",
   generation_config=generation_config,
 )
 
